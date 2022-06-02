3abee0ad5542e62b90856203f621ae03ba1106e4
Marcos Iturriaga <marcositu6@gmail.com>
2022-06-01 20:05:01 -0700

    finnished basic html started with SCSS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
 import "./Home.scss";
 import { Component } from "react";
 import axios from "axios";
-import Header from "../components/Header/Header";
-import Button from "../components/Button/Button";
+import heroIMG from "../assets/images/heroIMG.png";
+import headshot from "../assets/images/headshot.png";
+import { ImPlay, ImLinkedin, ImMail } from "react-icons/im";
 
 class Home extends Component {
   render() {
@@ -11,7 +12,7 @@ class Home extends Component {
         <nav className="home">
           <section className="home-hero">
             <div className="home-hero-text">
-              <h1 className="home-hero-text__name">Hello! my name is marcos</h1>
+              <h1 className="home-hero-text__name">Hello! my name is Marcos</h1>
               <h3 className="home-hero-text__description">
                 Web developer in Vancouver with a background in graphic design
               </h3>
@@ -20,11 +21,47 @@ class Home extends Component {
               <img
                 className="home-hero-imgph__img"
                 alt="hero image"
-                src="../assets/images/heroIMG.png"
+                src={heroIMG}
               ></img>
             </div>
           </section>
-          <section className="home-description"></section>
+          <section className="home-description">
+            <div className="home-description-imgph">
+              <img
+                className="home-description-imgph__img"
+                alt="hero image"
+                src={headshot}
+              ></img>
+            </div>
+            <div className="home-description-socials">
+              <h3 className="home-description-socials__title">Socials</h3>
+              <a
+                className="home-description-socials__youtube"
+                href="https://www.youtube.com/channel/UC9w82fODcKZ3ZBYavH71XgQ"
+              >
+                {" "}
+                <ImPlay />
+              </a>
+              <a
+                className="home-description-socials__linkedin"
+                href="https://www.youtube.com/channel/UC9w82fODcKZ3ZBYavH71XgQ"
+              >
+                {" "}
+                <ImLinkedin />
+              </a>
+              <a
+                className="home-description-socials__mail"
+                href="mailto:marcositu6@gmail.com"
+              >
+                {" "}
+                <ImMail />
+              </a>
+            </div>
+            <div className="home-description-text">
+              <h5 className="home-description-text__title">About me</h5>
+              <p className="home-description-text__paragraph"></p>
+            </div>
+          </section>
         </nav>
       </>
     );
